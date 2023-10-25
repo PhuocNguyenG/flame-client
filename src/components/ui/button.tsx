5239828d1cbc3bc2f499b74e76aab96920c1833f
Phuoc Nguyen <nguyenphuock18@gmail.com>
2023-10-25 20:51:55 +0700

    fix onlychildren button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -54,33 +54,35 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         ref={ref}
         {...props}
       >
-        {loading ? (
-          <div
-            className={`w-fit items-center font-semibold text-lg rounded-full transition ease-in-out duration-150 h-full mr-[3px]`}
-          >
-            <svg
-              className="animate-spin h-full w-fit text-primary"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
+        <>
+          {loading ? (
+            <div
+              className={`w-fit items-center font-semibold text-lg rounded-full transition ease-in-out duration-150 h-full mr-[3px]`}
             >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
-          </div>
-        ) : null}
-        <span className="w-fit whitespace-nowrap">{children}</span>
+              <svg
+                className="animate-spin h-full w-fit text-primary"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+            </div>
+          ) : null}
+          <div className="w-fit whitespace-nowrap">{children}</div>
+        </>
       </Comp>
     );
   }
