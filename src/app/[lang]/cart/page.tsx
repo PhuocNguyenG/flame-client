fd9bff775fbb60b013685803369585049dabfc6d
Phuoc Nguyen <nguyenphuock18@gmail.com>
2023-11-02 00:57:18 +0700

    cart dynamic

--- src/app/[lang]/cart/page.tsx
+++ src/app/[lang]/cart/page.tsx
@@ -1,8 +1,9 @@
-'use client'
 
 import ComingSoon from "@/components/coming-soon";
 import { Locale } from "@/lib/i18n/setting";
 import React from "react";
+
+export const dynamic = "force-dynamic"
 
 export default function Page({
   params: { lang },
